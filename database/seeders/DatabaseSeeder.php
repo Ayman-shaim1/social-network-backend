6b8f96d251acd47137b0ca3ac08ad5115a7ef935
Ayma-shaim1 <ayman.shaim10@gmail.com>
2023-03-29 02:07:56 +0000

    keep working on seeder & start creating controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,49 +13,59 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+
     public function run()
     {
+
         $users = [
             [
                 "id" => 1,
                 'name' => 'Jhon Doe',
                 'email' => 'john@example.com',
                 'password' => Hash::make('123456'),
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
+
             ],
             [
                 "id" => 2,
                 'name' => 'Steve Smith',
                 'email' => 'steve@example.com',
                 'password' => Hash::make('123456'),
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 3,
                 'name' => 'Richard Roe',
                 'email' => 'richard@example.com',
                 'password' => Hash::make('123456'),
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ]
         ];
 
         $posts = [
             [
                 "id" => 1,
-                "user_id" => $users[0]['id'],
-                "content" =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates ipsa eveniet non, reiciendis culpa vero voluptatibus cupiditate debitis incidunt inventore molestias pariatur? Corrupti aperiam hic maxime, sunt id labore?'
+                "user_id" => 1,
+                "content" =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates ipsa eveniet non, reiciendis culpa vero voluptatibus cupiditate debitis incidunt inventore molestias pariatur? Corrupti aperiam hic maxime, sunt id labore?',
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 2,
-                "user_id" => $users[1]['id'],
-                "content" =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates ipsa eveniet non, reiciendis culpa vero voluptatibus cupiditate debitis incidunt inventore molestias pariatur? Corrupti aperiam hic maxime, sunt id labore?'
+                "user_id" => 2,
+                "content" =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates ipsa eveniet non, reiciendis culpa vero voluptatibus cupiditate debitis incidunt inventore molestias pariatur? Corrupti aperiam hic maxime, sunt id labore?',
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 3,
-                "user_id" => $users[2]['id'],
-                "content" =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates ipsa eveniet non, reiciendis culpa vero voluptatibus cupiditate debitis incidunt inventore molestias pariatur? Corrupti aperiam hic maxime, sunt id labore?'
+                "user_id" => 3,
+                "content" =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates ipsa eveniet non, reiciendis culpa vero voluptatibus cupiditate debitis incidunt inventore molestias pariatur? Corrupti aperiam hic maxime, sunt id labore?',
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
         ];
 
@@ -63,40 +73,54 @@ class DatabaseSeeder extends Seeder
             // post 1 :
             [
                 "id" => 1,
-                "user_id" => $users[0]['id'],
-                "post_id" => $posts[0]['id'],
+                "user_id" => 1,
+                "post_id" => 1,
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 2,
-                "user_id" => $users[1]['id'],
-                "post_id" => $posts[0]['id'],
+                "user_id" => 2,
+                "post_id" => 1,
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 3,
-                "user_id" => $users[2]['id'],
-                "post_id" => $posts[0]['id'],
+                "user_id" => 3,
+                "post_id" => 1,
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             // post 2 :
             [
                 "id" => 4,
-                "user_id" => $users[0]['id'],
-                "post_id" => $posts[1]['id'],
+                "user_id" => 1,
+                "post_id" => 2,
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 5,
-                "user_id" => $users[1]['id'],
-                "post_id" => $posts[1]['id'],
+                "user_id" => 2,
+                "post_id" => 2,
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             // post 3 :
             [
-                "id" => 5,
-                "user_id" => $users[0]['id'],
-                "post_id" => $posts[2]['id'],
+                "id" => 6,
+                "user_id" => 1,
+                "post_id" => 3,
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
-                "id" => 6,
-                "user_id" => $users[1]['id'],
-                "post_id" => $posts[2]['id'],
+                "id" => 7,
+                "user_id" => 2,
+                "post_id" => 3,
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
         ];
 
@@ -104,47 +128,61 @@ class DatabaseSeeder extends Seeder
             // post 1 :
             [
                 "id" => 1,
-                "user_id" => $users[0]['id'],
-                "post_id" => $posts[0]['id'],
+                "user_id" => 1,
+                "post_id" => 1,
                 "content" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, natus!",
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 2,
-                "user_id" => $users[1]['id'],
-                "post_id" => $posts[0]['id'],
+                "user_id" => 2,
+                "post_id" => 1,
                 "content" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, natus!",
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 3,
-                "user_id" => $users[2]['id'],
-                "post_id" => $posts[0]['id'],
+                "user_id" => 3,
+                "post_id" => 1,
                 "content" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, natus!",
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             // post 2 :
             [
                 "id" => 4,
-                "user_id" => $users[0]['id'],
-                "post_id" => $posts[1]['id'],
+                "user_id" => 1,
+                "post_id" => 2,
                 "content" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, natus!",
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
                 "id" => 5,
-                "user_id" => $users[1]['id'],
-                "post_id" => $posts[1]['id'],
+                "user_id" => 2,
+                "post_id" => 2,
                 "content" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, natus!",
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             // post 3 :
             [
-                "id" => 5,
-                "user_id" => $users[0]['id'],
-                "post_id" => $posts[2]['id'],
+                "id" => 6,
+                "user_id" => 1,
+                "post_id" => 3,
                 "content" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, natus!",
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
             [
-                "id" => 6,
-                "user_id" => $users[1]['id'],
-                "post_id" => $posts[2]['id'],
+                "id" => 7,
+                "user_id" => 2,
+                "post_id" => 3,
                 "content" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, natus!",
+                "created_at" =>  date("Y-m-d H:i:s"),
+                "updated_at" => date("Y-m-d H:i:s"),
             ],
         ];
 
@@ -153,4 +191,12 @@ class DatabaseSeeder extends Seeder
         Like::insert($likes);
         Comment::insert($comments);
     }
+
+    // public function run()
+    // {
+    //     Comment::truncate();
+    //     Post::truncate();
+    //     Like::truncate();
+    //     User::truncate();
+    // }
 }
